5442ca459642a9847062ba29eb546a19953cf72d
Isaac Weisberg <net.caroline.weisberg@gmail.com>
2023-09-10 23:45:19 +0300

    Implement dismissal that even looks semi-good

--- src/ModalStack.ts
+++ src/ModalStack.ts
@@ -10,6 +10,7 @@ export interface IModalStack {
     root: HTMLElement
 
     present(c: IController): void
+    dismiss(c: IController): void
 }
 
 const sheetTransition = 'border-radius .2s ease-out, transform .2s ease-out'
@@ -26,9 +27,16 @@ export function ModalStack(initialController: IController): IModalStack {
         })
     }
 
+    function scrollToDismissedState(frame: ModalStackFrame) {
+        frame.sheetContainer.scrollTo({ 
+            top: 0, 
+            behavior: 'smooth'
+        })
+    }
+
     let frames: ModalStackFrame[] = []
 
-    const root = document.createElement('div')
+    const root = document.createElement('div') as HTMLDivElement
     root.style.backgroundColor = '#000000'
 
     root.style.height = baseHeight
@@ -54,7 +62,7 @@ export function ModalStack(initialController: IController): IModalStack {
             const firstTierConstrictionToFullSizeRatio = firstTierConstictionWidth / rootWidth
 
             if (frames.length > 0) {
-                const lastFrame = frames[frames.length - 1] 
+                const lastFrame = frames[frames.length - 1]
 
                 const elementThatWillFirstTierConstrict = lastFrame.sheetContainer
                 elementThatWillFirstTierConstrict.style.borderRadius = '24px'
@@ -104,6 +112,54 @@ export function ModalStack(initialController: IController): IModalStack {
             root.appendChild(sheetContainer)
 
             scrollToOpenedState(frame)
+        },
+        dismiss(c: IController): void {
+            const frameIdx = frames.findIndex((frame) => {
+                return frame.controller === c
+            })
+
+            if (frameIdx < 0) {
+                return
+            }
+
+            const framesThatStay = frames.slice(0, frameIdx)
+            const framesThatGo = frames.slice(frameIdx, frames.length)
+            let topFrameThatGoes: ModalStackFrame|undefined
+            if (framesThatGo.length > 0 ) {
+                topFrameThatGoes = framesThatGo[framesThatGo.length - 1]
+            } else {
+                topFrameThatGoes = undefined
+            }
+            let nonTopFramesThatGo: ModalStackFrame[]
+            if (framesThatGo.length > 1) {
+                nonTopFramesThatGo = framesThatGo.slice(0, framesThatGo.length - 1)
+            } else {
+                nonTopFramesThatGo = []
+            }
+
+            if (frameIdx > 0) {
+                const previousFrame = frames[frameIdx - 1]
+
+                previousFrame.sheetContainer.style.borderRadius = '0px'
+                previousFrame.sheetContainer.style.transform = ''
+            } else {
+                initialControllerWrapper.style.borderRadius = '0px'
+                initialControllerWrapper.style.transform = ''
+            }
+
+            for (const frame of nonTopFramesThatGo) {
+                root.removeChild(frame.sheetContainer)
+            }
+
+            if (topFrameThatGoes) {
+                const frame = topFrameThatGoes
+                scrollToDismissedState(frame)
+                setTimeout(() => {
+                    root.removeChild(frame.sheetContainer)
+                }, 700)
+            }
+
+            frames = framesThatStay
         }
     }
 }
