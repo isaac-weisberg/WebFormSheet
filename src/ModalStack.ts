62eadeac18fdaebcaeb3e38d53efd591f425d48a
Isaac Weisberg <net.caroline.weisberg@gmail.com>
2023-09-10 16:14:31 +0300

    Add properly working animation for pushing a controller

--- src/ModalStack.ts
+++ src/ModalStack.ts
@@ -3,6 +3,24 @@ import { IController } from "./IController"
 interface ModalStackFrame {
     controller: IController
     sheetContainer: HTMLDivElement
+    sheet: HTMLDivElement
+    contentWrapper: HTMLDivElement
+}
+
+function scrollToOpenedState(frame: ModalStackFrame) {
+    frame.contentWrapper.style.height = `calc(100vh - ${sheetGapFromTop}px)`
+    frame.sheetContainer.scrollTo({ 
+        top: frame.contentWrapper.offsetHeight, 
+        behavior: 'smooth'
+    })
+}
+
+function scrollToConstrictionState(frame: ModalStackFrame) {
+    frame.contentWrapper.style.height = `100vh`
+    frame.sheetContainer.scrollTo({ 
+        top: frame.contentWrapper.offsetHeight + sheetGapFromTop,
+        behavior: 'smooth'
+    })
 }
 
 export interface IModalStack {
@@ -10,6 +28,13 @@ export interface IModalStack {
 
     present(c: IController): void
 }
+
+const sheetsOriginTransform = '50% 0%'
+const sheetTransition = 'border-radius .2s ease-out, transform .2s ease-out'
+const sheetGapFromTop = 32
+const sheetsBackgroundColor = '#FFFFFF'
+
+let once = true
 
 export function ModalStack(initialController: IController): IModalStack {
     let frames: ModalStackFrame[] = []
@@ -23,8 +48,9 @@ export function ModalStack(initialController: IController): IModalStack {
 
     const initialControllerWrapper = document.createElement('div')
     initialControllerWrapper.style.overflow = 'hidden'
-    initialControllerWrapper.style.transition = 'border-radius .2s ease-out, transform .2s ease-out'
-    initialControllerWrapper.style.transformOrigin = '50% 25%'
+    initialControllerWrapper.style.transition = sheetTransition
+    initialControllerWrapper.style.transformOrigin = sheetsOriginTransform
+    initialControllerWrapper.style.backgroundColor = sheetsBackgroundColor
     root.appendChild(initialControllerWrapper)
 
     initialController.view.style.height = baseHeight
@@ -35,31 +61,34 @@ export function ModalStack(initialController: IController): IModalStack {
     return {
         root,
         present(c: IController): void {
-            let elementThatWillFirstTierConstrict: HTMLDivElement
-            if (frames.length > 0) {
-                const lastFrame = frames[frames.length - 1] 
-                elementThatWillFirstTierConstrict = lastFrame.sheetContainer
-            } else {
-                elementThatWillFirstTierConstrict = initialControllerWrapper
-            }
-
             const rootWidth = root.offsetWidth
             const firstTierConstictionWidth = rootWidth - firstTierConstrictionOffsetFromEdge * 2
             const firstTierConstrictionToFullSizeRatio = firstTierConstictionWidth / rootWidth
-            elementThatWillFirstTierConstrict.style.borderRadius = '24px'
-            elementThatWillFirstTierConstrict.style.transform = `scale(${firstTierConstrictionToFullSizeRatio}) translate(0px,  -8px)`
+
+            if (frames.length > 0) {
+                const lastFrame = frames[frames.length - 1] 
+
+                const elementThatWillFirstTierConstrict = lastFrame.sheetContainer
+                elementThatWillFirstTierConstrict.style.transformOrigin = sheetsOriginTransform
+                elementThatWillFirstTierConstrict.style.borderRadius = '24px'
+                elementThatWillFirstTierConstrict.style.transform = `translate(0px,  24px) scale(${firstTierConstrictionToFullSizeRatio})`
+                scrollToConstrictionState(lastFrame)
+            } else {
+                const elementThatWillFirstTierConstrict = initialControllerWrapper
+                elementThatWillFirstTierConstrict.style.transformOrigin = sheetsOriginTransform
+                elementThatWillFirstTierConstrict.style.borderRadius = '24px'
+                elementThatWillFirstTierConstrict.style.transform = `translate(0px,  24px) scale(${firstTierConstrictionToFullSizeRatio})`
+            }
 
             const sheetContainer = document.createElement('div')
             sheetContainer.style.position = 'absolute'
             sheetContainer.style.top = '0px'
             sheetContainer.style.left = '0px'
             sheetContainer.style.right = '0px'
-            sheetContainer.style.transition = 'border-radius .2s ease-out, transform .2s ease-out'
+            sheetContainer.style.transition = sheetTransition
             sheetContainer.className = '_modalContainer'
             sheetContainer.style.height = baseHeight
             sheetContainer.style.overflow = 'scroll'
-
-            root.appendChild(sheetContainer)
 
             const sheet = document.createElement('div')
             sheet.className = '_sheet'
@@ -71,7 +100,7 @@ export function ModalStack(initialController: IController): IModalStack {
 
             const contentWrapper = document.createElement('div')
             contentWrapper.className = '_contentWrapper'
-            contentWrapper.style.height = `calc(${baseHeight} - 32px)`
+            contentWrapper.style.height = `calc(${baseHeight} - ${sheetGapFromTop}px)`
             contentWrapper.style.backgroundColor = "#FFFFFF"
             contentWrapper.style.overflow = 'scroll'
             sheet.appendChild(contentWrapper)
@@ -80,16 +109,15 @@ export function ModalStack(initialController: IController): IModalStack {
 
             const frame: ModalStackFrame = {
                 controller: c,
-                sheetContainer
+                sheetContainer,
+                sheet,
+                contentWrapper
             }
-            
+        
             frames = frames.concat(frame)
             root.appendChild(sheetContainer)
 
-            sheetContainer.scrollTo({ 
-                top: contentWrapper.offsetHeight, 
-                behavior: 'smooth'
-            })
+            scrollToOpenedState(frame)
         }
     }
 }
